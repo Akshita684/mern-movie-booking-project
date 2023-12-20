16a38ee7b13feb52f383e9ede2eb9da44847082c
Akshita684 <150109811+Akshita684@users.noreply.github.com>
2023-12-20 15:51:51 +0530

    Delete backend directory

--- backend/routes/booking-routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const bookingRouter = express.Router;
-
-bookingRouter.post('/', Booking);
-bookingRouter.delete('/:id', deleteBooking);
-
-module.exports = bookingRouter;
--- backend/routes/movie-routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const movieRouter = express.Router;
-
-movieRouter.get('/', getAllMovie);
-movieRouter.post('/add', addMovie);
-movieRouter.get('/:id', getMovieById);
-
-module.exports = movieRouter;
